6c4e4468478c521d024ea6f363b4b0d78dff9f5a
beduin <beduin94@mail.ru>
2020-05-26 12:50:23 +1000

    общая структура приложения

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShopsComponent } from './shops/shops.component';
+import { SpecialistsComponent } from './specialists/specialists.component';
+import { SaveFormComponent } from './save-form/save-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopsComponent,
+    SpecialistsComponent,
+    SaveFormComponent
   ],
   imports: [
     BrowserModule
