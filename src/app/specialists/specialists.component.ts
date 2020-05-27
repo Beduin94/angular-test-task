de9acfb2772984f345de78daa0d727cdcb1ebd3b
beduin <beduin94@mail.ru>
2020-05-27 21:51:04 +1000

    добавил окончания

--- src/app/specialists/specialists.component.ts
+++ src/app/specialists/specialists.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {DataService} from "../services/data.service";
+import {ISpecialist} from "../services/request.service";
 
 @Component({
   selector: 'app-specialists',
@@ -9,5 +10,11 @@ import {DataService} from "../services/data.service";
 export class SpecialistsComponent {
 
   constructor(public dataService: DataService) { }
+
+  getText(count:number):string{
+    const cases = [2, 0, 1, 1, 1, 2];
+    const titles = ['магазин', 'магазина', 'магазинов'];
+    return titles[ (count%100>4 && count%100<20)? 2 : cases[(count%10<5)?count%10:5] ];
+  }
 
 }
